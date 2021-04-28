ca982f78770888af02a7e525cc7273fdbb2fe4f0
Rajani Raju <rajaniraju2003@gmail.com>
2021-04-28 14:41:45 -0500

    prag_replace()

--- welcome.php
+++ welcome.php
@@ -7,21 +7,33 @@
 
     while (!feof($fn)) {
         // Read 1 line at a time.
-        $result = fgets($fn);
+        $result['result'] = fgets($fn);
 
         // Split the line by tab.
-        //$parts = preg_split('/\s+/', $result);
+
+        $parts = preg_split('/\s+/', $result['result']);
+
+
 
         // Process each word
 
         // Add procesed word to another string
-        echo $result;
+        echo $result['result'] . '<br>';
+        echo '<pre>';
+        var_dump($parts) . '<br>';
+        echo '</pre>';
+        //Use the preg_replace function to remove any characters that are not letters, hyphens, or single spaces from the FIRST and LAST names.
+
+        $array = preg_replace("/\d?\dIPT\.\w/", "IPT", $parts);
+        echo '<pre>';
+        var_dump($array) . '<br>';
+        echo '</pre>';
     }
-    
+
     fclose($fn);
 
     // Write processed string into a new file.
-    
+
     ?>
 
 
